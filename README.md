530acff04545c23a7134a8cd23378dd8c9aff999
boonprakit <boonprakit@screencloud.io>
2021-06-03 21:15:05 +0700

    update script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Local Trust Certificates
+This allows you to run your local web server with a valid HTTPs but please note that this custom certificate will be valid only on the same machine that generate the cert
+
+## How to run
+```
+> sudo ./run.sh example.com v1.example.com v2.example.com
+```
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-openssl req -x509 -nodes -new -sha256 -days 999999 -newkey rsa:2048 -keyout RootCA.key -out RootCA.pem -subj "/C=US/CN=Example-Root-CA"
+openssl req -x509 -nodes -new -sha256 -days 360 -newkey rsa:2048 -keyout RootCA.key -out RootCA.pem -subj "/C=US/CN=Example-Root-CA"
 openssl x509 -outform pem -in RootCA.pem -out RootCA.crt
 
 cat > domains.ext << EOF
@@ -9,18 +9,18 @@ basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
 subjectAltName = @alt_names
 [alt_names]
-
-DNS.1 = doszy-dev.com
-DNS.2 = app.doszy-dev.com
-DNS.3 = api.doszy-dev.com
-DNS.4 = initial-app.doszy-dev.com
-DNS.5 = initial-app2.doszy-dev.com
-DNS.6 = app-3rd.doszy-dev.com
-DNS.7 = flex-simulator.doszy-dev.com
 EOF
 
+i=1;
+for url in "$@" 
+do
+    echo "DNS.${i} = ${url}" >> domains.ext
+    i=$((i + 1));
+    shift 1;
+done
+
 openssl req -new -nodes -newkey rsa:2048 -keyout localhost.key -out localhost.csr -subj "/C=US/ST=YourState/L=YourCity/O=Example-Certificates/CN=localhost.local"
-openssl x509 -req -sha256 -days 1024 -in localhost.csr -CA RootCA.pem -CAkey RootCA.key -CAcreateserial -extfile domains.ext -out localhost.crt
+openssl x509 -req -sha256 -days 360 -in localhost.csr -CA RootCA.pem -CAkey RootCA.key -CAcreateserial -extfile domains.ext -out localhost.crt
 
 if [ "$(uname)" == "Darwin" ]; then
     sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ./RootCA.pem
@@ -31,4 +31,4 @@ fi
 
 sudo rm localhost.csr RootCA.srl
 
-echo "Now, you should be able to serve your local server with localhost.crt and localhost.key"
+echo "Now you can serve your local server with localhost.crt and localhost.key"
